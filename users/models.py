a31837786edd4692b239b691850b5945f9612938
hujiDemocracy <hujidemocracygame@gmail.com>
2025-04-18 11:56:46 +0300

    created models.py, done migrations, added tests, requirements and README

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id = models.CharField(max_length=9, primary_key=True)  # Israeli ID
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
